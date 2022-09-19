8b356efc338289523806b31d15729713e367c108
reeshunt <kmr.rishabh6190@gmail.com>
2022-09-19 04:47:01 -0700

    merge varun-niec

--- WeCharge_AdminPanel/Controllers/HomeController.cs
+++ WeCharge_AdminPanel/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using System.Diagnostics;
